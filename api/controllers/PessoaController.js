1b58c87497b0e5b470925e10f172b24f75f47ab6
André Sampaio <andresampaio@id.uff.br>
2023-05-31 14:35:39 -0300

    GET one profile from pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -3,11 +3,25 @@ const database = require('../models')
 class PessoaController {
     static async pegaTodasAsPessoas(req,res){
         try{
-            const todasAsPessoas = await database.Pessoas.findAll()
-            return res.status(200).json(todasAsPessoas)
+            const todasAsPessoas = await database.Pessoas.findAll();
+            return res.status(200).json(todasAsPessoas);
         }catch(error){
-            return res.status(500).json(error.message)
+            return res.status(500).json(error.message);
         }   
+    }
+
+    static async pegaUmaPessoa(req,res){
+        const { id } = req.params;
+        try{
+            const umaPessoa = await database.Pessoas.findOne({
+                where: { 
+                    id : Number(id) 
+                }
+            })
+            return res.status(200).json(umaPessoa);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
     }
 }
 
