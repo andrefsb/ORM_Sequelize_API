4e503206136bff7481e1e773b4e2e03ecd04fca8
André Sampaio <andresampaio@id.uff.br>
2023-05-31 16:01:51 -0300

    PUT & DELETE profile in pessoas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -33,6 +33,28 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async atualizaPessoa(req,res){
+        const { id } = req.params;
+        const novasInfos = req.body;
+        try{
+            await database.Pessoas.update(novasInfos,{where: {id : Number(id)}});
+            const pessoaAtualizada= await database.Pessoas.findOne({where: {id : Number(id)}});
+            return res.status(200).json(pessoaAtualizada);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaPessoa(req,res){
+        const { id } = req.params;
+        try{
+            await database.Pessoas.destroy({where: {id : Number(id)}});
+            return res.status(200).json({mensagem: `Id ${id} successfully deleted.`});
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
