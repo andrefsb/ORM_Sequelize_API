17bf67242370828872c31747f78aec866df249b4
André Sampaio <andresampaio@id.uff.br>
2023-06-05 22:39:35 -0300

    PUT and DELETE registration

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -81,6 +81,33 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async atualizaMatricula(req,res){
+        const { estudanteId, matriculaId } = req.params;
+        const novasInfos = req.body;
+        try{
+            await database.Matriculas.update(novasInfos,{
+                where: {
+                    id : Number(matriculaId),
+                    estudante_id: Number (estudanteId)
+                }
+            });
+            const matriculaAtualizada= await database.Matriculas.findOne({where: {id : Number(matriculaId)}});
+            return res.status(200).json(matriculaAtualizada);
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async apagaMatricula(req,res){
+        const { estudanteId, matriculaId } = req.params;
+        try{
+            await database.Matriculas.destroy({where: {id : Number(matriculaId)}});
+            return res.status(200).json({mensagem: `Registration ${matriculaId} from student ${estudanteId} successfully deleted.`});
+        }catch(error){
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController;
